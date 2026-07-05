02f74106fa257a93df008da6ec71bf7febc61981
448 OG <superuser@448.africa>
2026-07-05 10:42:06 +0300

    Remove cors in prod by checking debug assertions

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,14 +3,18 @@ use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
+    #[cfg(debug_assertions)]
     let origins = [
         "http://localhost:5173".parse().unwrap(),
         "http://127.0.0.1:5173".parse().unwrap(),
     ];
 
+    #[cfg(debug_assertions)]
     let cors = CorsLayer::new()
         .allow_origin(origins)
         .allow_methods([Method::GET, Method::POST]);
+    #[cfg(not(debug_assertions))]
+    let cors = CorsLayer::new();
 
     let app = Router::new().route("/", get(root)).layer(cors);
 
